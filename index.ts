0a140c7a2a373473718082139ac44a9960a68491
tinusgraglin <tinusgraglin@gmail.com>
2024-02-04 14:51:20 +0800

    new functionality: simple visit statistics

--- index.ts
+++ index.ts
@@ -10,16 +10,41 @@ function serve_blog(path: String): Response {
     return new Response(Bun.file(maybe_file))
 }
 
+let visit_log: Map<string, [Date, string][]> = new Map()
+
+function visit_log_dump(): string {
+    const s: string[] = []
+    visit_log.forEach((val, key) => {
+        s.push(key + ':\n  ' + val.join('\n  '))
+    })
+    return s.join('\n')
+}
+
 Bun.serve({
-    hostname: '0.0.0.0',
+    hostname: '127.0.0.1',
     port: '23333',
-    fetch(request, _server) {
-        const url = new URL(request.url)
-        const path = url.pathname
-        if (path === '/blog') {
+    fetch(request, server) {
+        const req_url = new URL(request.url)
+        const req_path = req_url.pathname
+
+        const req_ip = server.requestIP(request)
+        if (req_ip !== null) {
+            const from = req_ip.address + ':' + req_ip.port
+            const new_entry: [Date, string] = [new Date(), req_path]
+            const lst = visit_log.get(from)
+            if (lst === undefined) {
+                visit_log.set(from, [new_entry])
+            } else {
+                lst.push(new_entry)
+            }
+        }
+
+        if (req_path === '/statistics') {
+            return new Response(visit_log_dump())
+        } else if (req_path === '/blog') {
             return serve_blog('/')
-        } else if (path.startsWith('/blog/')) {
-            return serve_blog(path.substring(5))
+        } else if (req_path.startsWith('/blog/')) {
+            return serve_blog(req_path.substring(5))
         } else {
             return new Response(Bun.file('home/index.html'))
         }
